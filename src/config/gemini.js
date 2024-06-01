306d765e07b71c2ecbff0bcaf4e2b061f2b993b9
Furkan Doğu <furkandogu2018@gmail.com>
2024-06-01 23:07:45 +0300

    gemini api den gelen veri ekrana bastırıldı

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -57,6 +57,7 @@ import {
   
     const result = await chatSession.sendMessage(prompt);
     console.log(result.response.text());
+    return result.response.text()
   }
   
   export default run;
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,19 +1,42 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini"
 
 export const Context = createContext();
 
-const ContextProvider = ({ children }) => {
+const ContextProvider = (props) => {
+
+    const [input, setInput] = useState("")
+    const [recentPrompt, setRecentPrompt] = useState("")
+    const [prevPropmts, setPrevPropmts] = useState([])
+    const [showResult, setShowResult] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [resultData, setResultData] = useState("")
 
     const onSent = async (prompt) => {
-        await run(prompt)
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+        const response = await run(input)
+        setResultData(response)
+        setLoading(false)
+        setInput("")
     }
 
-    onSent("what is react js")
+    const values = {
+        prevPropmts,
+        setPrevPropmts,
+        showResult,
+        onSent,
+        recentPrompt,
+        setRecentPrompt,
+        loading,
+        resultData,
+        input,
+        setInput
+    };
 
-    const values = {};
-
-    return <Context.Provider value={values}>{children}</Context.Provider>;
+    return <Context.Provider value={values}>{props.children}</Context.Provider>;
 };
 
 export default ContextProvider;
