9e3790f455e9ae84565713735954706296cf4be1
Furkan Doğu <furkandogu2018@gmail.com>
2024-06-01 21:00:00 +0300

    home oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Home />
     </>
   )
 }
